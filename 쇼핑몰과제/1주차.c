f85a61e25cdf5405dcb88e04ca5cc4c77f7f8418
Kimarg <sa5615347@naver.com>
2024-11-20 20:36:00 +0900

    Update 1주차.c

--- 쇼핑몰과제/1주차.c
+++ 쇼핑몰과제/1주차.c
@@ -1,35 +1,25 @@
 #include <stdio.h>
+#define SIZE 100
 int main()
 {
-    int item[99];
-    int count = 0;
     int i = 0;
-    int a = 0;
-    printf("상품종류를 입력하세요 : ");
-    scanf("%d",&count);
-    printf("상품 별 입고수량을 입력해주세요 : ");
-    for (i = 0; i < count; i++)
-    {
-        scanf("%d",&item[i]);
-    }
-    //printf("%d %d %d %d %d",item[0],item[1],item[2],item[3],item[4]); <===== cheak code
-    printf("상품 별 판매수량을 입력해주세요 : ");
-    for (i = 0; i < count; i++)
-    {
-        scanf("%d",&a);
-        item[i] -= a;
-    }
-    printf("ID 입력하세요 : ");
-    scanf("%d",&a);
-    a -= 1;
-    //printf("%d",a); <===== cheak code
-    printf("아이디 재고 수량입니다.\n%d\n",item[a]);
+    int item_type = 0;
+    int item_ID = 0;
+    int put_item[SIZE] = {0};
+    int sell_item[SIZE] = {0};
     
-    for (i = 0; i < count; i++)
-    {
-        printf("%d ",item[i]);
+    scanf("%d",&item_type);
+    for(i = 0;i<item_type;i++){
+        scanf("%d",&put_item[i]);
     }
-    printf("모든 제품의 재고 수량입니다.");
+    for(i = 0;i<item_type;i++){
+        scanf("%d",&sell_item[i]);
+    }
+    scanf("%d",&item_ID);
+    printf("\n%d\n",put_item[item_ID]-sell_item[item_ID]);
+    for(i = 0;i<item_type;i++){
+        printf("%d ",put_item[i]-sell_item[i]);
+    }
     
-    return 0;
+return 0;
 }
